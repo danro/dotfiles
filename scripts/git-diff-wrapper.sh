5383958c894a08142ff96d4d0fe22d4700e387ec
Dan Rogers <dan.rogers@bkwld.com>
2012-10-09 17:51:59 -0700

    OSX defaults / external git diff config

--- scripts/git-diff-wrapper.sh
+++ scripts/git-diff-wrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # uses FileMerge which is included in Xcode dev tools.
-/usr/bin/opendiff "$2" "$5" -merge "$1"
+# xcrun -r opendiff "$2" "$5" -merge "$1"
+/usr/local/bin/ksdiff "$2" "$5"
