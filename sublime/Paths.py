7f21f0b81fffe4b903d89026be6adbbfb1a8dfef
Dan Rogers <dan.rogers@bkwld.com>
2013-01-29 15:07:16 -0800

    Simplify jshint path

--- sublime/Paths.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Add paths for node scripts
-LOCAL = '/usr/local/bin:/usr/local/sbin:'
-
-# Sublime's default path is
-# /usr/bin:/bin:/usr/sbin:/sbin
-os.environ['PATH'] += ':'
-os.environ['PATH'] += LOCAL
