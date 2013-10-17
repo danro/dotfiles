314936e64491d92642ed8b3f9850cba8f760e0f4
Dan Rogers <dan.rogers@bkwld.com>
2013-10-16 21:30:05 -0700

    Remove some old scripts

--- scripts/git-diff-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# uses FileMerge which is included in Xcode dev tools.
-# xcrun -r opendiff "$2" "$5" -merge "$1"
-# /usr/local/bin/ksdiff "$2" "$5"
-
-# Kaleidoscope config
-/usr/local/bin/ksdiff --partial-changeset --relative-path "$1" -- "$2" "$5"
